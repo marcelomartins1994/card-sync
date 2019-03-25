7f1cad02f25a02ebd5e5ad71465ca1e1aea5d8d2
Marcelo <marcelo.martins@liberedu.com>
2019-03-25 16:44:07 -0300

    .

--- js/client.js
+++ js/client.js
@@ -24,7 +24,7 @@ TrelloPowerUp.initialize({
                 icon: SEND_TO_SPRINT_ICON,
                 text: "Send To Sprint",
                 callback: function(t) {
-                    return t.card('name').get('name').set("TESTANDO, GAROTADA!");
+                    return t.set('card', 'shared','name','TESTANDO, PORRA!');
                 }
             },
             {
