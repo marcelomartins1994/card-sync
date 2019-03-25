ad988ad44429b4097ff819d782a8063c429a12a9
Marcelo <marcelo.martins@liberedu.com>
2019-03-25 18:47:43 -0300

    .

--- js/client.js
+++ js/client.js
@@ -25,9 +25,7 @@ TrelloPowerUp.initialize({
                 text: "Send To Sprint",
                 callback: function(t) {
                     
-                    document.getElementsByClassName("card-detail-window u-clearfix")[0].
-                    getElementsByClassName("window-title")[0].
-                    getElementsByClassName("card-detail-title-assist js-title-helper")[0].textContent = "É AGORA, SENHORES!"
+                    document.getElementsByClassName("card-detail-window u-clearfix")[0].getElementsByClassName("window-title")[0].getElementsByClassName("card-detail-title-assist js-title-helper")[0].textContent = "É AGORA, SENHORES!"
                     
                 }
             },
