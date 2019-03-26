39ad490a2356b05f3d0e8a241096219bf5307164
Marcelo <marcelo.martins@liberedu.com>
2019-03-26 17:47:28 -0300

    .

--- js/client.js
+++ js/client.js
@@ -29,7 +29,7 @@ TrelloPowerUp.initialize({
                 text: "Send To Sprint",
                 callback: function(t) {
                     
-                    var cardID = t.card('id').get('id').get('_settledValue')
+                    var cardID = t.card('id').get('id').get('settledValue')
                     console.log(cardID)
                     /*
                     var data = null
