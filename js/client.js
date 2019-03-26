d14d375813474aeb9a675ad4f73220c530a344b0
Marcelo <marcelo.martins@liberedu.com>
2019-03-26 17:25:14 -0300

    .

--- js/client.js
+++ js/client.js
@@ -1,8 +1,12 @@
 var Promise = TrelloPowerUp.Promise;
 
-var SEND_TO_SPRINT_ICON = "./images/send_to_sprint_icon.svg" // INCLUDE PATH TO FILE HERE
-var CARD_SYNC_ICON = "./images/sync_icon.svg" //INCLUDE PATH TO FILE HERE
+var SEND_TO_SPRINT_ICON = "./images/send_to_sprint_icon.svg" 
+var CARD_SYNC_ICON = "./images/sync_icon.svg"
 
+const API_KEY = ''
+const TOKEN = ''
+
+const CARD_EDIT_URL = "https://api.trello.com/1/cards/"
 
 //function sendToSprint(t) {
 
@@ -24,11 +28,25 @@ TrelloPowerUp.initialize({
                 icon: SEND_TO_SPRINT_ICON,
                 text: "Send To Sprint",
                 callback: function(t) {
-                    //var name =  'BOOOOORA! VAI SUBIR EM ÁRVORE É O CARALHO!'
-                    //t.card('name') = name
-                    var name = t.card('name')
                     
+                    var name = t.card('id')
                     console.log(name)
+                    /*
+                    var data = null
+                    var xhr = new XMLHttpRequest()
+
+                    xhr.addEventListener(
+                        "readystatechange", function() {
+
+                            if (this.readyState === this.DONE) {
+                                console.log(this.responseText)
+                            }
+                        }
+                    )
+                    */
+                    //xhr.open("PUT", CARD_EDIT_URL + cardID + "?name=" + newName + "&key=" + API_KEY + "&token=" + TOKEN)
+                    //xhr.send(data)
+
                     return name
                 }
             },
